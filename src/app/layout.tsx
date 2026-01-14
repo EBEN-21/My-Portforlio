5206637a168df7f158642085e2d2a30ee0cd13e6
Ebenezer Nwikina <ebennwik@gmail.com>
2026-01-14 15:59:37 +0100

    Added metadata to layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,11 @@ import { Inter, Poppins } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 const poppins = Poppins({ subsets: ['latin'], weight: ['400','600','700'], variable: '--font-poppins' })
 
+export const metadata = {
+  title: 'Ebenezer Nwikina | Front Developer', 
+  description: 'The portfolio website of Ebenezer Nwikina, a Frontend Web Developer specializing in creating stunning and responsive web applications using modern technologies.',
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${inter.variable} ${poppins.variable}`}>
